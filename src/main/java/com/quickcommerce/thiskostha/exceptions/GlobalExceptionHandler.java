4eb9f7295c0b9e00122a5a1b2dc12a7a3936b7e1
sai-0406 <mskrishna0406@gmail.com>
2026-03-27 17:05:30 +0530

    commited

--- src/main/java/com/quickcommerce/thiskostha/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/quickcommerce/thiskostha/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,14 @@
 package com.quickcommerce.thiskostha.exceptions;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	  @ExceptionHandler(RestaurantNotFoundException.class)
+	    public ResponseEntity<String> handleRestaurantNotFound(RestaurantNotFoundException ex) {
+	        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	    }
 }
